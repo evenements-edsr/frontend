89b69d534fd3b92800617dd8b29a2db60e5c407c
Valentin Maerten <valentin@maerten.fr>
2018-07-01 17:57:59 +0200

    [FEAT] : Initialisation du projet Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edsr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'edsr';
+}
