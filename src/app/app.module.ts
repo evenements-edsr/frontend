39e90c1f2164e9c3b73b0bbb57d20fc706e4fe71
Valentin Maerten <valentin@maerten.fr>
2018-09-23 20:28:33 +0200

    [FEAT] : ajout de la page de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule }    from '@angular/platform-browser';
 import { NgModule }         from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent }     from './app.component';
+import { LoginModule }      from './login/login.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { AppComponent }     from './app.component';
   ],
   imports: [
     BrowserModule,
+    LoginModule,
     AppRoutingModule,
   ],
   providers: [],
