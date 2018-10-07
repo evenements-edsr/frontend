0e194672fdb81b227264e92c3f9f0a6ead1fd647
Valentin Maerten <valentin@maerten.fr>
2018-10-07 17:42:19 +0200

    [FEAT] : ajout du calendar Bulma

--- src/app/login/pages/inscription/inscription.component.ts
+++ src/app/login/pages/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import bulmaCalendar from 'bulma-calendar/dist/js/bulma-calendar.min.js';
 
 @Component({
   selector: 'edsr-inscription',
@@ -10,6 +11,16 @@ export class InscriptionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Initialize all input of date type.
+    const calendars = bulmaCalendar.attach('[type="date"]', {lang : 'fr'});
+
+// Loop on each calendar initialized
+    calendars.forEach(calendar => {
+      // Add listener to date:selected event
+      calendar.on('date:selected', date => {
+        console.log(date);
+      });
+    });
   }
 
 }
