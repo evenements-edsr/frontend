665f80ff78f7e382859dbf4e13542c55406ebdd6
Valentin Maerten <valentin@maerten.fr>
2018-10-08 21:37:37 +0200

    [FEAT] : ajout de la validation sur le login form

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,12 +4,14 @@ import { InscriptionComponent } from './pages/inscription/inscription.component'
 import { LoginComponent }       from './pages/login/login.component';
 import { FontAwesomeModule }    from '@fortawesome/angular-fontawesome';
 import { CommonModule }         from '@angular/common';
+import { ReactiveFormsModule }  from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     LoginRoutingModule,
     FontAwesomeModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     LoginComponent,
--- src/app/login/pages/login/login.component.ts
+++ src/app/login/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }                  from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'edsr-login',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted: boolean = false;
+
+  loginValidationMessage = {
+    'email': [
+      { type: 'required', message: 'L\'adresse email est obligatoire' },
+      { type: 'email', message: 'L\'adresse n\'est pas une adresse email valide' }
+    ],
+    'password': [
+      { type: 'required', message: 'Le mot de passe est obligatoire' },
+    ],
+  };
+
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', [Validators.required]]
+    });
+  }
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    alert('SUCCESS!! :-)')
+  }
+
+  shouldDisplayErrors(field: string) {
+    return (!this.loginForm.get(field).valid && this.loginForm.get(field).touched) ||
+      (this.loginForm.get(field).untouched && this.submitted);
   }
 
 }
