79478f296dbeb170181686ffa44b17e6c58d510e
Valentin Maerten <valentin@maerten.fr>
2018-10-10 20:21:30 +0200

    [FEAT] : ajout d'une icone de profil dans la navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent }     from './app.component';
 import { LoginModule }      from './login/login.module';
 
-import { FontAwesomeModule }                 from '@fortawesome/angular-fontawesome';
-import { library }                           from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
-import { NavbarComponent }                   from './navbar/navbar.component';
-import { faFacebook, faGoogle, faTwitter }   from '@fortawesome/free-brands-svg-icons';
-import { DisplayErrorsComponent }            from './ui/display-errors/display-errors.component';
-import { EventsModule }                      from './events/events.module';
+import { FontAwesomeModule }                               from '@fortawesome/angular-fontawesome';
+import { library }                                         from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faExclamationTriangle, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { NavbarComponent }                                 from './navbar/navbar.component';
+import { faFacebook, faGoogle, faTwitter }                 from '@fortawesome/free-brands-svg-icons';
+import { EventsModule }                                    from './events/events.module';
 
-library.add(faEnvelope,faExclamationTriangle, faGoogle, faFacebook, faTwitter);
+library.add(faEnvelope,faExclamationTriangle, faGoogle, faFacebook, faTwitter, faUserCircle);
 
 @NgModule({
   declarations: [
