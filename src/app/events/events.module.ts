6c6697ccc3dadf5c4963fdf13dddc553ef59d022
Valentin Maerten <valentin@maerten.fr>
2018-10-13 13:50:21 +0200

    [FEAT-WIP] : Proposition label sur box pour evenements

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -2,11 +2,13 @@ import { NgModule }            from '@angular/core';
 import { CommonModule }        from '@angular/common';
 import { EvenementsComponent } from './pages/evenements/evenements.component';
 import { EventsRoutingModule } from './events-routing.module';
+import { FontAwesomeModule }   from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   imports: [
     CommonModule,
     EventsRoutingModule,
+    FontAwesomeModule,
   ],
   declarations: [EvenementsComponent]
 })
